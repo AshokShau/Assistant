638b3a9e71467b560256dabd2086f7496b126e33
Ashok Bishnoi <114943948+Abishnoi69@users.noreply.github.com>
2024-01-26 23:17:47 +0530

    some cmd only for sudo

--- Assistant/setup.py
+++ Assistant/setup.py
@@ -1,7 +1,11 @@
-from telegram.ext import Application, CommandHandler, ExtBot, MessageHandler, filters
+from telegram.ext import Application
+from telegram.ext import CommandHandler as CH
+from telegram.ext import ExtBot
+from telegram.ext import MessageHandler as MH
+from telegram.ext import filters
 
-from Assistant import SUDO_USERS
-from Assistant.modules.broadcast import broadcast, stats
+from Assistant import SUDO_USERS as SUDO
+from Assistant.modules.broadcast import broadcast, logs, stats
 from Assistant.modules.messages import incoming_groups, incoming_private
 from Assistant.modules.start import start
 from Assistant.modules.sudos import block_func, mode_func, unblock_func
@@ -9,20 +13,21 @@ from Assistant.modules.sudos import block_func, mode_func, unblock_func
 
 async def setup_(application: Application[ExtBot]) -> None:
 
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("stats", stats))
-    application.add_handler(CommandHandler("broadcast", broadcast))
-    application.add_handler(CommandHandler("mode", mode_func))
-    application.add_handler(CommandHandler("block", block_func))
-    application.add_handler(CommandHandler("unblock", unblock_func))
+    application.add_handler(CH("start", start))
+    application.add_handler(CH("logs", logs, filters=filters.User(SUDO)))
+    application.add_handler(CH("stats", stats, filters=filters.User(SUDO)))
+    application.add_handler(CH("broadcast", broadcast, filters=filters.User(SUDO)))
+    application.add_handler(CH("mode", mode_func, filters=filters.User(SUDO)))
+    application.add_handler(CH("block", block_func, filters=filters.User(SUDO)))
+    application.add_handler(CH("unblock", unblock_func, filters=filters.User(SUDO)))
     application.add_handler(
-        MessageHandler(
-            filters.ChatType.GROUPS & filters.User(SUDO_USERS) & ~filters.COMMAND,
+        MH(
+            filters.ChatType.GROUPS & filters.User(SUDO) & ~filters.COMMAND,
             incoming_groups,
         ),
         group=1,
     )
     application.add_handler(
-        MessageHandler(filters.ChatType.PRIVATE & ~filters.COMMAND, incoming_private),
+        MH(filters.ChatType.PRIVATE & ~filters.COMMAND, incoming_private),
         group=2,
     )
