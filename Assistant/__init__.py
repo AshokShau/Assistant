1b1f788c49db43032589e5eef861d1d0fc138fad
Ashok Bishnoi <114943948+Abishnoi69@users.noreply.github.com>
2024-02-10 10:12:19 +0530

    nice one

--- Assistant/__init__.py
+++ Assistant/__init__.py
@@ -1,27 +1,19 @@
 import logging
+import config
+
 from time import time
 
-from config import DEBUG, SUDO_USER
 
-if DEBUG:
-    logging.basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-        level=logging.DEBUG,
-    )
-else:
-    logging.basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-        level=logging.INFO,
-    )
-    logging.getLogger("apscheduler").setLevel(logging.WARNING)
-    logging.getLogger("httpx").setLevel(logging.ERROR)
-
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=config.LOGGER_LEVEL,
+)
+logging.getLogger("apscheduler").setLevel(logging.WARNING)
+logging.getLogger("httpx").setLevel(logging.ERROR)
 
 LOGGER = logging.getLogger(__name__)
 
 
 StartTime = time()
-
-SUDO_USERS = SUDO_USER
+SUDO_USERS = config.SUDO_USER
